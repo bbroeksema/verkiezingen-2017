5c6fe49767f70539c483a347d602f6d63443c9c4
Bertjan Broeksema <bertjan.broeksema@bigdatarepublic.nl>
2017-02-24 21:36:58 +0100

    Make JSLint happy
    We had some issues with Safari. JSLint does not give any errors on
    this code anymore. So let's see if Safari is also happy.
    
    Additionally, we make use of JSON.stringify to see if we can make
    the requests to backend more robust

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -1,23 +1,34 @@
-var btn = document.getElementById('button_submit');
-var input = document.getElementById('input_query');
+var btn = document.getElementById("button_submit");
+var input = document.getElementById("input_query");
 var radarChart;
 
 function render(data) {
-    data = data.sort(function(a, b) { return b[1] - a[1]; })
-    var radarData = { labels : data.map(function(d) { return d[0]; }),
-        datasets : [
+    "use strict";
+
+    data = data.sort(function (a, b) {
+        return b[1] - a[1];
+    });
+    var radarData = {
+        labels: data.map(function (d) {
+            return d[0];
+        }),
+        datasets: [
             {
                 label: "Uw termen komen overeen met",
-                fillColor : "rgba(220,220,220,0.5)",
-                strokeColor : "rgba(220,220,220,1)",
-                data : data.map(function(d) { return d[1]; })
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,1)",
+                data: data.map(function (d) {
+                    return d[1];
+                })
             }
         ]
-    }
+    };
     var ctx = document.getElementById("radarChart").getContext("2d");
-    if (radarChart) radarChart.destroy()
+    if (radarChart) {
+        radarChart.destroy();
+    }
     radarChart = new Chart(ctx, {
-        type: 'radar',
+        type: "radar",
         data: radarData,
         options: {
             scale: {
@@ -29,14 +40,18 @@ function render(data) {
     });
 }
 
-btn.addEventListener('click', function(event) {
-    var text = input.value.replace(/(\r\n|\n|\r|\"|\&)/gm," ");
-    var query = "{ \"text\": \"" + text + "\" }"
-
+btn.addEventListener("click", function () {
+    "use strict";
+    var text = input.value.replace(/(\r\n|\n|\r|["'&])/gm, " ");
+    var query = JSON.stringify({ "text": text });
 
     d3.request("/fit")
         .header("Content-Type", "application/json")
-        .on("error", function(error) { console.log(error); })
-        .on("load", function(xhr) { render(JSON.parse(xhr.responseText)); })
+        .on("error", function (error) {
+            console.log(error);
+        })
+        .on("load", function (xhr) {
+            render(JSON.parse(xhr.responseText));
+        })
         .post(query);
 });
